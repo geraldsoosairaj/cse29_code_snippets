9749bc5cd8460087211d2ba44fe1fa879137f90c
Gerald Soosairaj <asoosairaj@ucsd.edu>
2024-04-25 09:40:54 -0700

    Add lecture4 files.

--- lecture4/characters.c
+++ lecture4/characters.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int letter = '3';
-    // ASCII value for the letter '0' is 48
-    printf("%c  %d  %x\n", letter, letter, letter);
-    
+    int character = '3';
+    // ASCII value for the character '0' is 48
+    printf("%c  %d  %x\n", character, character, character);
+
     int hex = 0x41;
     printf("%x %d %c\n", hex, hex, hex);
 
-    char ch = 'a'; 
-    printf("%c\n", ch + 1);
+    char ch = 'b'; 
+    printf("%c %c %c\n", (ch + 1), (ch - 1), ch);
     return 0;
 }
 
--- lecture4/integers.c
+++ lecture4/integers.c
@@ -5,10 +5,18 @@ int main()
 {
     int max_signed_int = INT_MAX;
     int min_signed_int = INT_MIN;
-    int max_unsigned_int = UINT_MAX;
+    unsigned int max_unsigned_int = UINT_MAX;
+    long max_signed_long_int = LONG_MAX;
+    long min_signed_long_int = LONG_MIN;
+    unsigned long max_unsigned_long_int = ULONG_MAX;
+
     printf("max_signed_int = %d\n", max_signed_int);
     printf("min_signed_int = %d\n", min_signed_int);
     printf("max_unsigned_int = %u\n", max_unsigned_int);
+    printf("----------------------------------------------------\n");
+    printf("max_signed_long_int = %ld\n", max_signed_long_int);
+    printf("min_signed_long_int = %ld\n", min_signed_long_int);
+    printf("max_unsigned_long_int = %lu\n", max_unsigned_long_int);
     return 0;
 }
 
